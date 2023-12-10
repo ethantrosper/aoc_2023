93ed9bbbef9a4d5e85ad40e7a4c92da8594e689e
Ethan Trosper <ethancoletrosper@gmail.com>
2023-12-09 20:18:47 -0500

    added in node_modules

--- day_1/dayOne.ts
+++ day_1/dayOne.ts
@@ -1,6 +1,7 @@
+import * as fsPromise from 'fs/promises';
 /*
-Given many lines of input find the Calibration value
-Calibration value is the sum of all ints concatnated with the int to the right
+TASK:
+Concat the first and last digit of each line into a number then add all the numbers
 */
 
 function concatnate(numOne:  string, numTwo: string) : string{
@@ -21,26 +22,6 @@ function isNum(char: string) : boolean{
 }
 
 function main(){
-    var input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
-    var sum = 0;
-
-    const stringOfNumbers: string[] = [];
-
-    for(let i = 0; i < input.length; i++ ){
-        var currChar = input.charAt(i);
-
-        if( isNum(currChar) ){
-            stringOfNumbers.push(currChar);
-        }
-
-    }
-    var total = 0;
-    for(let i = 0; i < stringOfNumbers.length; i += 2){
-        var sNum = concatnate(stringOfNumbers[i], stringOfNumbers[i+1]);
-        console.log(sNum);
-        total += parseInt(sNum);
-    }
-
-    console.log(total);
+    
 }
 main();
